c9d2ebcfcbfe4e1f156ade73c93ebba64dcf94e1
aufe1995 <aufe1995@gmail.com>
2023-06-30 00:33:08 +0900

    로그인 및 회원가입 페이지 수정

--- src/pages/JoinPage.js
+++ src/pages/JoinPage.js
@@ -1,23 +1,69 @@
 import './JoinPage.css'
+import React, { useState } from 'react';
 
 
 function JoinPage() {
+    const [id, setId] = useState("");
+    const [password, setPassword] = useState("");
+
+    const onChangeId = (e) => {
+        const currentId = e.target.value;
+        setId(currentId);
+        const idRegExp = /^[a-zA-z0-9]{3,12}$/;
+     
+        if (!idRegExp.test(currentId)) {
+            
+        } else {
+
+        }
+    };
+
+    const onChangePassword = (e) => {
+        const currentPassword = e.target.value;
+        setPassword(currentPassword);
+        const passwordRegExp = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,14}$/;
+     
+        if (!passwordRegExp.test(currentPassword)) {
+
+        } else {
+
+        }
+    };
+
     return (
         <div className='JoinPageArea'>
             <div className='JoinArea'>
                 <form>
-                    <div> 회원가입 </div>
-                    <div> 아이디 </div>
-                    <input />
-                    <button>중복확인</button>
-                    <div> 비밀번호 </div>
-                    <input />
-                    <div> 비밀번호 확인 </div>
-                    <input />
-                    <div></div>
-                    <button> 회원가입 </button>
-                    <button>취소</button>
+                    <div className='JoinTitle'> 회원가입 </div>
+                    <hr/>
+                    <div className='JoinContent'>
+                        <div className='JoinContentTitle'> 아이디 </div>
+                        <input name="id" onChange={onChangeId} />
+                        <button className='IdCheckButton'>중복확인</button>
+                        <div className='JoinContentDesc' name="idDesc" >* 아이디는 영문 또는 숫자만 입력해주세요.</div>
+
+                        <div className='JoinContentTitle'> 비밀번호 </div>
+                        <input name="password" onChange={onChangePassword} />
+                        <div className='JoinContentDesc'>* 비밀번호는 영문, 숫자, 특수문자가 모두 포함되게 입력해주세요.</div>
+
+                        <div className='JoinContentTitle'> 비밀번호 확인 </div>
+                        <input name="passwordConfirm" />
+
+                        <div className='JoinContentTitle'> 이름 </div>
+                        <input name="name"  />
+
+                        <div className='JoinContentTitle'> 전화번호 </div>
+                        <input name="phoneNumber"  />
+
+                        <div className='JoinContentTitle'> 주소 </div>
+                        <input name="address" />
+                        <div></div>
+
+                        <button className='JoinButton'> 회원가입 </button>
+                        <button className='JoinButton'>취소</button>
+                    </div>
                 </form>
+
             </div>
         </div>
     );
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -5,13 +5,16 @@ function LoginPage() {
         <div className='LoginPageArea'>
             <div className='LoginArea'>
                 <form>
-                    <div> 로그인 </div>
-                    <div> 아이디 </div>
-                    <input />
-                    <div> 비밀번호 </div>
-                    <input />
-                    <div></div>
-                    <button> 로그인 </button>
+                    <div className='LoginTitle'> 로그인 </div>
+                    <hr/>
+                    <div className='LoginContent'>
+                        <div className='LoginContentTitle'> 아이디 </div>
+                        <input />
+                        <div className='LoginContentTitle'> 비밀번호 </div>
+                        <input />
+                        <div></div>
+                        <button> 로그인 </button>
+                    </div>
                 </form>
             </div>
         </div>
