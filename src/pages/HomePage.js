18984cc9ed3baee3a0b73b2f589bb8a58f5470ab
aufe1995 <aufe1995@gmail.com>
2023-07-04 18:14:49 +0900

    modify token unit

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -10,43 +10,43 @@ function HomePage() {
                     <img src={Box} alt='productImg' className='productImg' />
                     <div>다이아 상자</div>
                     <div>블록체인 기반 랜덤상자</div>
-                    <div>50,000 Token</div>
+                    <div>50,000 Luck</div>
                 </Link>
                 <Link to={"/product"} className='productList'>
                     <img src={Box} alt='productImg' className='productImg' />
                     <div>플래티넘 상자</div>
                     <div>블록체인 기반 랜덤상자</div>
-                    <div>10,000 Token</div>
+                    <div>10,000 Luck</div>
                 </Link>
                 <Link to={"/product"} className='productList'>
                     <img src={Box} alt='productImg' className='productImg' />
                     <div>골드 상자</div>
                     <div>블록체인 기반 랜덤상자</div>
-                    <div>5,000 Token</div>
+                    <div>5,000 Luck</div>
                 </Link>
                 <Link to={"/product"} className='productList'>
                     <img src={Box} alt='productImg' className='productImg' />
                     <div>실버 상자</div>
                     <div>블록체인 기반 랜덤상자</div>
-                    <div>1,000 Token</div>
+                    <div>1,000 Luck</div>
                 </Link>
                 <Link to={"/product"} className='productList'>
                     <img src={Box} alt='productImg' className='productImg' />
                     <div>브론즈 상자</div>
                     <div>블록체인 기반 랜덤상자</div>
-                    <div>500 Token</div>
+                    <div>500 Luck</div>
                 </Link>
                 <Link to={"/product"} className='productList'>
                     <img src={Box} alt='productImg' className='productImg' />
                     <div>아이언 상자</div>
                     <div>블록체인 기반 랜덤상자</div>
-                    <div>100 Token</div>
+                    <div>100 Luck</div>
                 </Link>
                 <Link to={"/product"} className='productList'>
                     <img src={Box} alt='productImg' className='productImg' />
                     <div>무료 상자</div>
                     <div>블록체인 기반 랜덤상자</div>
-                    <div>0 Token</div>            
+                    <div>0 Luck</div>            
                 </Link>
             </div>
         </div>
